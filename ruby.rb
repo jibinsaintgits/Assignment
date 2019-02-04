76e5ba238fa8748010e505a97fa7c9060e53e38e
jibinsaintgits <37616574+jibinsaintgits@users.noreply.github.com>
2019-02-04 06:38:47 +0000

    Add files via upload

--- ruby.rb
+++ ruby.rb
@@ -1,4 +1,5 @@
 class Student
+  @@no_of_stud = 0
    def initialize( rollno,name, mark1, mark2)
       @srollno = rollno
       @sname = name
@@ -11,12 +12,17 @@ class Student
       puts "Student Name is #@sname"
       puts "Subject 1 mark is #@smark1"
       puts "Subject 2 mark is #@smark2"
+       @@no_of_stud += 1
      
    end
       def avg()
        puts "Average mark of #@name is #{(@smark1 + @smark2) / 2}"
 
        end
+       def total_no_of_stud
+      puts "Total number of Student is #@@no_of_stud"
+end
+      
 end
 stud1= Student.new("1","Appu",40,45)
 stud2= Student.new("2","Allen",31,35)
@@ -26,5 +32,6 @@ stud1.display_details()
 stud2.display_details()
 stud3.display_details()
 stud1.avg()
+stud1.total_no_of_stud
 
 
